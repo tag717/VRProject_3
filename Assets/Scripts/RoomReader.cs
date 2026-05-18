189986adabc1c33999999d0a2f2a352a50c4eaef
tag717 <tayoon@ucsd.edu>
2026-05-17 22:43:10 -0700

    did task2

--- Assets/Scripts/RoomReader.cs
+++ Assets/Scripts/RoomReader.cs
@@ -5,9 +5,24 @@ public class RoomReader : MonoBehaviour
 {
     public LayerMask surfaceLayer;
 
-    private void OnEnable()
+    private void Start() //used to be onEnable() but changed
     {
+        if (MRUK.Instance == null) return;
         MRUK.Instance.SceneLoadedEvent.AddListener(OnSceneLoaded);
+
+        if (MRUK.Instance.GetCurrentRoom() != null)
+        {
+            OnSceneLoaded();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Use OnDestroy to clean up the listener instead of OnDisable
+        if (MRUK.Instance != null)
+        {
+            MRUK.Instance.SceneLoadedEvent.RemoveListener(OnSceneLoaded);
+        }
     }
 
     private void OnDisable()
@@ -19,16 +34,43 @@ public class RoomReader : MonoBehaviour
     private void OnSceneLoaded()
     {
         var room = MRUK.Instance.GetCurrentRoom();
-
+        if (room == null) return;
+        
         var floor = room.FloorAnchor;
-        floor.gameObject.layer = LayerMask.NameToLayer("Surface");
-
+        if (floor != null)
+        {
+            floor.gameObject.layer = LayerMask.NameToLayer("Surface");
+            EnsureCollider(floor);
+        }
         foreach (var wall in room.WallAnchors)
         {
             wall.gameObject.layer = LayerMask.NameToLayer("Surface");
 
             if (wall.gameObject.GetComponent<Collider>() == null)
                 wall.gameObject.AddComponent<BoxCollider>();
+                EnsureCollider(wall);
+        }
+        Debug.Log("Room loaded and surfaces mapped!");
+    }
+
+    // Helper method to accurately size colliders based on the anchor data
+    private void EnsureCollider(MRUKAnchor anchor)
+    {
+        // Only add a collider if one doesn't exist
+        if (anchor.gameObject.GetComponent<Collider>() == null)
+        {
+            // Walls and floors use 2D planes in the Scene API
+            if (anchor.HasPlane)
+            {
+                BoxCollider bc = anchor.gameObject.AddComponent<BoxCollider>();
+                
+                // Scale the collider to match the physical plane's dimensions
+                // We make the Z-axis (thickness) 0.01f so it acts as a solid surface
+                bc.size = new Vector3(anchor.PlaneRect.Value.width, anchor.PlaneRect.Value.height, 0.01f);
+                
+                // Center the collider on the plane
+                bc.center = new Vector3(anchor.PlaneRect.Value.center.x, anchor.PlaneRect.Value.center.y, 0);
+            }
         }
     }
 }
